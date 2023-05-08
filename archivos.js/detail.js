22eb2b6e16b445949fd5b73048a0fe4eea85c48f
Natalia001-bz <125935088+Natalia001-bz@users.noreply.github.com>
2023-05-08 01:45:17 -0500

    filtros, funcione js
    se agregan filtros, funciones js, se quitan archivos no necesarios

---
+++ archivos.js/detail.js
@@ -0,0 +1,70 @@
+
+
+
+
+async function apiFetch() {
+  let slectId= new URLSearchParams (
+    window.location.search)
+  const id = slectId.get("id")
+  //  console.log(id)
+  
+  try {
+
+    let response = await fetch( `https://pro-talento.up.railway.app/api/amazing/${id}`)
+
+    response = await response.json()
+ // console.log(response)
+ var dateEvento = new Date(response.response.date).toISOString().slice(0, 10);
+//  console.log(dateEvento)
+ 
+  
+  let contenedorDetail= document.getElementById("cardDetails")
+  let cardDetail = document.createElement("div")
+
+  cardDetail.innerHTML = `
+  <div class="card mb-3" style="  ">
+  <div class="row">
+    <div class="col-md-4 ">
+  <img src="${response.response.image}" class="img-fluid rounded-start  imgdetail">
+  </div>
+  <div class="col-md-8">
+  <div class="card-body" >
+    <h5 class="card-title">${response.response.name}</h5><br>
+    <p class="card-text linea ">${response.response.description}</p> <br> 
+       <hr class="lineatop w-50">
+    <p>Date: ${dateEvento}</p>   
+  <p>Category: ${response.response.category}</p>
+  <p>Place: ${response.response.place}</p>
+  <hr class="lineatop w-50">
+  <p>price: $ ${response.response.price}</p> 
+  <p>${response.response.assistance?"Assistance: " + response.response.assistance:"Estimate: " + response.response.estimate}</p>
+   
+  </div>
+</div>
+</div>
+</div>
+  `
+  contenedorDetail.appendChild(cardDetail)
+ 
+   //  printCardDetails()
+
+  
+  
+  } 
+  catch (error) {
+    console.log(error)
+  }
+
+}
+apiFetch()
+
+
+
+
+
+
+
+
+
+
+
--- archivos.js/stats.js
+++ archivos.js/stats.js
@@ -1,153 +1,145 @@
 
 
 function template(arrayDatos) {
-    let cards = ` 
-    <div class="card m-5" style="width: 26rem; height: 650px ">
-    <img src="${arrayDatos.image}" class="card-img-top imgCard" alt="${arrayDatos.id}">
-    <div class="card-body m-1 fs-5">
-      <h5 class="card-title">${arrayDatos.name}</h5>
-      <p class="card-text mb-1">${arrayDatos.description}</p>
-      <p class="card-text ">Precio : $ ${arrayDatos.price}</p>
-      <a href="./detail.html" class="btn btn-dark "> Details </a>
-        </div>
+  let cards = ` 
+  <div class="card m-5" style="width: 26rem; height: 650px ">
+  <img src="${arrayDatos.image}" class="card-img-top imgCard" alt="${arrayDatos.id}">
+  <div class="card-body m-1 fs-5">
+    <h5 class="card-title">${arrayDatos.name}</h5>
+    <p class="card-text mb-1">${arrayDatos.description}</p>
+    <p class="card-text ">Precio : $ ${arrayDatos.price}</p>
+     <a href="./detail.html?id=${arrayDatos.id}" class="btn btn-dark onclick" id="${arrayDatos.id}"> Details </a>
       </div>
-        </div>
-  </div>  `
-  
-    return cards
-  
-  }
-  
-  
-  
-  
-  
-  function printCards(arrayDatos, etiqueta_seleccionada) {
-    let allCards = []
-    for (let elemento of arrayDatos) {
-      allCards.push(template(elemento))
-    }
-    // console.log(allCards)
-    allCards = allCards.join('')
-    // console.log(allCards)
-    let selector = document.getElementById(etiqueta_seleccionada)
-    // console.log(selector)
-    selector.innerHTML = allCards
-  }
-  // printCards(arrayDatos,'cards')
-  
-  
-  
-  let apiUrl = "https://pro-talento.up.railway.app/api/amazing/"
-  
-  async function apiFetch(apiUrl) {
-    try {
-  
-      let response = await fetch(apiUrl)
-  
-      response = await response.json()
-      // console.log(response)
-      // console.log(response.response)
-      let arrayCategory = [...new Set(response.response.map(each => each.category))]
-      console.log(arrayCategory)
-      // ____________________________
-  
-      const allCategory = document.getElementById('divChecks')
-      const generarChecks = []
-      for (let i = 0; i < response.response.length; i++) {
-        const category = response.response[i].category
-        if (!generarChecks.includes(category)) {
-          generarChecks.push(category)
-          allCategory.innerHTML += `
-             
-              <div class="d-flex class_checked  justify-content-start" id="divChecks">
-                <div class="col-md-12">
-              <div class="form-check form-check-inline">
-                      <input class="form-check-input fs-3" type="checkbox" id="inlineCheckbox" value="${category}">
-                      <label class="form-check-label fs-3" for="${category}">${category}</label>
-                    </div>
-                    </div>
-                    </div>
-                    
-                     `
-        }
-      }
-  
-      let todoslosCheckbox = document.querySelectorAll(".form-check-input")
-  todoslosCheckbox.forEach( each =>{
-    each.addEventListener("change", function(e){
-  cardsFiltradas()
-    })
-  })
-  
-      // ____________________________
-      printCards(response.response, "cards")
-      
-    
-      // checksTrue(response)
-  
-      const inputTexto = document.querySelector('#buscador')
-          inputTexto.addEventListener('keyup', e => {
-      cardsFiltradas()   
-      })
-    
-    } 
-    catch (error) {
-      console.log(error)
-    }
-  
-  }
-  apiFetch(apiUrl)
-  
-  ________________________________________
-  async function cardsFiltradas() {
-    try {
-      console.log()
-  
-      let inputTexto = document.getElementById("buscador").value.toLowerCase()
-      let checks = Array.from(document.querySelectorAll('.form-check-input:checked')).map(e => e.value)
-       console.log(checks)
-      let url = `https://pro-talento.up.railway.app/api/amazing/?name=${inputTexto}&category=${checks.join(',')}`
-      let response = await fetch(url)
-      response = await response.json()
-       console.log(response.response)
-      if (response.response.length == 0) {
-        busquedaFallida('cards')
-  
-      }
-      else {
-        printCards(response.response, "cards")
-      }
-    }
-  
-  
-  
-    catch (error) {
-      console.log(error)
-    }
-  }
-  
-  
-  // ___________________________
-  // window.click_here = function(buttonId) {
-  //   window.location.href = "../detail.html"; // se trae la dirección local y se cambia a la dirección de details
-  //   window.idClickStr = buttonId  // Se almacena el valor del parámetro (buttonId)
-  //   sessionStorage.setItem("idClickStr",idClickStr) // Se almacena la información en el Storage
-  //  }
-  // ___________________________
-  
-  
-  function busquedaFallida(etiqueta_seleccionada) {
-    const divCards = document.getElementById(etiqueta_seleccionada)
-    divCards.innerHTML = `
-    <div class="alert alert-warning d-flex align-items-center" role="alert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-    <div>
-      <h3 class=" fs-3"> SEARCH FAILED!</h3>
     </div>
-  </div>
-    `
-    console.log(divCards)
+      </div>
+</div>  `
+
+  return cards
+
+}
+
+
+
+
+
+function printCards(arrayDatos, etiqueta_seleccionada) {
+  let allCards = []
+  for (let elemento of arrayDatos) {
+    allCards.push(template(elemento))
   }
-  
-  
+  // console.log(allCards)
+  allCards = allCards.join('')
+  // console.log(allCards)
+  let selector = document.getElementById(etiqueta_seleccionada)
+  // console.log(selector)
+  selector.innerHTML = allCards
+}
+// printCards(arrayDatos,'cards')
+
+
+
+let apiUrl = "https://pro-talento.up.railway.app/api/amazing/"
+
+async function apiFetch(apiUrl) {
+  try {
+
+    let response = await fetch(apiUrl)
+
+    response = await response.json()
+    // console.log(response)
+    // console.log(response.response)
+    let arrayCategory = [...new Set(response.response.map(each => each.category))]
+    // console.log(arrayCategory)
+    // ____________________________
+
+    const allCategory = document.getElementById('divChecks')
+    const generarChecks = []
+    for (let i = 0; i < response.response.length; i++) {
+      const category = response.response[i].category
+      if (!generarChecks.includes(category)) {
+        generarChecks.push(category)
+        allCategory.innerHTML += `
+           
+            <div class="d-flex class_checked  justify-content-start" id="divChecks">
+              <div class="col-md-12">
+            <div class="form-check form-check-inline">
+                    <input class="form-check-input fs-3" type="checkbox" id="inlineCheckbox" value="${category}">
+                    <label class="form-check-label fs-3" for="${category}">${category}</label>
+                  </div>
+                  </div>
+                  </div>
+                  
+                   `
+      }
+    }
+
+    let botonFecha = document.getElementById("selectFecha").addEventListener("change", cardsFiltradas)
+    let inputTexto = document.querySelector("#buscador").addEventListener("keyup", cardsFiltradas)
+    let todoslosCheckbox = document.querySelectorAll(".form-check-input")
+    todoslosCheckbox.forEach(each => {
+      each.addEventListener("change", function (e) {
+        cardsFiltradas()
+      })
+    })
+     // ____________________________
+    printCards(response.response, "cards")
+ 
+  }
+  catch (error) {
+    console.log(error)
+  }
+
+}
+apiFetch(apiUrl)
+
+
+
+// ________________________________________
+
+async function cardsFiltradas() {
+  try {
+    // console.log()
+    let selectDate = document.getElementById("selectFecha").value
+    let inputTexto = document.getElementById("buscador").value.toLowerCase()
+    let checks = Array.from(document.querySelectorAll('.form-check-input:checked')).map(e => e.value)
+
+    // console.log(checks)
+    let url = `https://pro-talento.up.railway.app/api/amazing/?name=${inputTexto}&category=${checks.join(',')}&time=${selectDate}`
+    let response = await fetch(url)
+    response = await response.json()
+    // console.log(response.response)
+
+
+    if (response.response.length == 0) {
+      busquedaFallida('cards')
+
+    }
+    else {
+      printCards(response.response, "cards")
+    }
+  }
+
+
+
+  catch (error) {
+    console.log(error)
+  }
+}
+
+
+// ___________________________
+function busquedaFallida(etiqueta_seleccionada) {
+  const divCards = document.getElementById(etiqueta_seleccionada)
+  divCards.innerHTML = `
+  <div class="alert alert-warning d-flex align-items-center" role="alert">
+  <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+  <div>
+    <h3 class=" fs-3"> SEARCH FAILED!</h3>
+  </div>
+</div>
+  `
+  // console.log(divCards)
+}
+
+
+
